3323d2f0534586f5d5ce4fa84d43edc76995a9b0
Josh Johnston <josh@x-team.com>
2017-08-07 13:40:40 +1000

    final tweaks

--- presentation/index.js
+++ presentation/index.js
@@ -37,7 +37,8 @@ cmz('spectacle-slide', `
 `).toString()
 
 const images = {
-  logo: require("../assets/x-team-logo-gray.svg"),
+  x: require('../assets/x.png'),
+  diary: require('../assets/diary.jpg'),
   tabs: require('../assets/tabs.gif'),
   typewriter: require('../assets/typewriter.gif'),
   gitBlame: require('../assets/git-blame.jpg'),
@@ -60,8 +61,8 @@ preloader(images);
 
 const colors = {
   primary: 'hsl(180, 10%, 90%)',
-  secondary: 'black',
-  tertiary: 'hsl(90, 10%, 10%)',
+  secondary: 'hsl(90, 10%, 10%)',
+  tertiary: 'hsl(290, 30%, 10%)',
   quarternary: 'hsl(120, 20%, 60%)'
 };
 
@@ -115,15 +116,55 @@ function Footer () {
   `
     background: #fff
     padding: 1rem
-    font-size: 1.5rem
+    font-size: 2rem
   `, 
   monospace
   ])
   return (
 <Outer>
-  <Inner><b>@joshwnj</b> - Dear Diary</Inner>
+  <Inner>
+    <b>@joshwnj</b> - 
+    Dear Diary
+  </Inner>
 </Outer>
   )
+}
+
+const lights = {
+  outer: cmz(`
+  display: flex
+  flex-direction: row
+  margin-left: -4vw
+`),
+
+  group: cmz(`
+  display: flex
+  flex-direction: column
+  margin-right: 2vw
+  padding: 1vw
+  background: #222
+  border-radius: 2vw
+`),
+
+  circle: cmz(`
+  border-radius: 50vw
+  width: 6vw
+  height: 6vw
+  background-color: black
+  margin-bottom: 1vw
+`),
+
+  c1: cmz(`
+  background: rgb(208, 216, 70)
+`),
+
+  c2: cmz(`
+  background: rgb(238, 239, 130)
+`),
+
+  c3: cmz(`
+  background: rgb(183, 174, 139)
+`),
 }
 
 export default class Presentation extends React.Component {
@@ -134,13 +175,41 @@ export default class Presentation extends React.Component {
       <Deck progress='none' transition={["fade"]} transitionDuration={500} theme={theme}>
         <Slide>
           <Heading>Dear Diary</Heading>
-          <Heading>~</Heading>
+
+          <div style={{margin: '5vh auto', width: '9vw', height: '9vw', padding: '2vw', borderRadius: '10vw', background: 'white', overflow: 'hidden'}} >
+            <Image src={images.x} style={{width: '100%'}} />
+          </div>
+
           <Heading size={3}><em>Journal writing for programmers</em></Heading>
+
+          <Notes>
+            <ul>
+              <li>We live in a time where sharing ideas...</li>
+              <li>But ideas are funny things. Sometimes there's a bad idea...</li>
+            </ul>
+          </Notes>
         </Slide>
 
         <Slide>
           <div className={cmz(['display: flex', 'text-align: left'])}>
+            {/*
             <Image src={images.traffic} style={{maxHeight: '90vh', marginLeft: '-8vw', marginTop: '-5vh', marginRight: '5vw'}} />
+            */}
+
+            <div className={lights.outer}>
+              <div className={lights.group}>
+                <div className={lights.circle} style={{background: 'red'}} />
+                <div className={lights.circle} style={{background: 'orange'}} />
+                <div className={lights.circle} style={{background: 'limegreen'}} />
+              </div>
+
+              <div className={lights.group}>
+                <div className={lights.circle + ' ' + lights.c1} />
+                <div className={lights.circle + ' ' + lights.c2} />
+                <div className={lights.circle + ' ' + lights.c3} />
+              </div>
+            </div>
+
             <Heading size={3}>Bad ideas can gain widespread adoption</Heading>
           </div>
         </Slide>
@@ -150,6 +219,16 @@ export default class Presentation extends React.Component {
             <Image src={images.ancientSpinner} style={{maxHeight: '90vh', marginLeft: '-16vw', marginTop: '-7vh', marginRight: '5vw'}} />
             <Heading size={3}>Good ideas can come before their time</Heading>
           </div>
+
+          <Notes>
+            <ul>
+              <li>So we're surrounded by ideas ... until you've spent some time taking them for a spin.</li>
+            </ul>
+          </Notes>
+        </Slide>
+
+        <Slide bgColor="tertiary">
+          <Heading textColor="primary"><em>&ldquo;I'll read that later&rdquo;</em></Heading>
         </Slide>
 
         <Slide bgImage={images.tabs} bgDarken={0.1}>
@@ -157,18 +236,25 @@ export default class Presentation extends React.Component {
 
         <Slide>
           <Heading>Keeping Up</Heading>
+          
+          <Notes>
+            <ul>
+              <li>It's fantastic there are so many people coming up with ideas...</li>
+              <li>But the sheer volume...</li>
+            </ul>
+          </Notes>
         </Slide>
 
         <Slide bgImage={images.fireman}>
         </Slide>
 
-        <Slide bgColor="secondary">
+        <Slide bgColor="tertiary">
           <BlockQuote>
             <Quote textColor="primary" style={{fontSize: '4rem'}}>there is increased evidence that we are being bogged down today as specialization extends. </Quote>
           </BlockQuote>
         </Slide>
 
-        <Slide bgColor="secondary">
+        <Slide bgColor="tertiary">
           <BlockQuote>
 
             <Quote textColor="primary" style={{fontSize: '4rem'}}>The investigator is staggered by the findings and conclusions of thousands of other workers - conclusions which he cannot find time to grasp, much less to remember</Quote>
@@ -192,12 +278,33 @@ export default class Presentation extends React.Component {
               <Bubble><img src={images.squeeze} /></Bubble>
             </div></Appear>
           </div>
+
+          <Notes>
+            <ul>
+              <li>When he wrote this, he was part of an international team of physicists...</li>
+              <li>So even back then he saw the beginning of this information explosion...</li>
+              <li>He wrote about the bitter-sweetness of his experience, Like...</li>
+            </ul>
+          </Notes>
+
         </Slide>
 
         <Slide bgImage={images.swords} style={{ backgroundPosition: 'center bottom' }}>
+          <Notes>
+            <ul>
+              <li>In the midst of the horror and destruction of war...</li>
+              <li>But even back then he knew that it wouldn't be enough...</li>
+            </ul>
+          </Notes>
         </Slide>
 
         <Slide bgImage={images.memex}>
+          <Notes>
+            <ul>
+              <li>At its heart, the memex is a story-telling device. The user of the memex...</li>
+              <li>I wonder, what do you think programming is? Is it more of a creative activity, or... ?</li>
+            </ul>
+          </Notes>
         </Slide>
 
         <Slide>
@@ -213,13 +320,26 @@ export default class Presentation extends React.Component {
           <Appear>
             <Image src={images.getCreative} />
           </Appear>
+
+          <Notes>
+            <ul>
+              <li>Get Creative... I would even go so far as to say that programming is a form of creative writing</li>
+            </ul>
+          </Notes>
         </Slide>
 
-        <Slide bgColor="secondary" textColor="primary">
+        <Slide bgColor="tertiary" textColor="primary">
           <BlockQuote>
             <Quote>You can make anything by writing</Quote>
             <Cite textColor="quarternary">C. S. Lewis</Cite>
           </BlockQuote>
+
+          <Notes>
+            <ul>
+              <li>You can make anything, because words are a primary...</li>
+              <li>To get the truth, you need both...</li>
+            </ul>
+          </Notes>
         </Slide>
 
         <Slide>
@@ -248,21 +368,41 @@ export default class Presentation extends React.Component {
             
             <CodePane
               style={{fontSize: '2rem', marginLeft: '-20vw', marginRight: '2vw'}}
-              lang="js"
+              lang="jsx"
+              source={require("raw-loader!./example-1")}
+              margin="20px auto" />
+
+          </div>
+          </Fill>
+        </Slide>
+
+        <Slide>
+          <Fill>
+          <div style={{display: 'flex'}}>
+            
+            <CodePane
+              style={{fontSize: '2rem', marginLeft: '-20vw', marginRight: '2vw'}}
+              lang="jsx"
               source={require("raw-loader!./example-1")}
               margin="20px auto" />
 
               <CodePane
                 style={{fontSize: '2rem'}}
-                lang="js"
+                lang="jsx"
                 source={require("raw-loader!./example-2")}
                 margin="20px auto" />
           </div>
           </Fill>
+
+          <Notes>
+            <ul>
+              <li>We really notice this when it's missing. Have you ever...</li>
+            </ul>
+          </Notes>
         </Slide>
 
-        <Slide bgImage={images.gitBlame} bgDarken={0.75}>
-          <Image src={images.gitBlame} />
+        <Slide bgColor="tertiary">
+          <Image src={images.gitBlame} style={{width: '100%'}} />
         </Slide>
 
         <Slide>
@@ -273,9 +413,16 @@ export default class Presentation extends React.Component {
             <Heading><em><b>coders:</b> learn to <b>write</b></em></Heading>
           </div></Appear>
         </Slide>
+        <Slide style={{backgroundColor: 'rgb(240, 225, 195)'}}>
+          <Heading style={{marginTop: '-15vh'}}>Getting started</Heading>
+          <Image src={images.diary} style={{width: '100%'}} />
 
-        <Slide>
-          <Heading>Getting started</Heading>
+          <Notes>
+            <ul>
+              <li>I'm trying to record that journey of all the small decisions and directions...</li>
+              <li>I have a few preferences of tools...</li>
+            </ul>
+          </Notes>
         </Slide>
 
         <Slide>
@@ -283,10 +430,23 @@ export default class Presentation extends React.Component {
           <Heading>~</Heading>
           <Appear><Heading size={4}>Fast & Furious</Heading></Appear>
           <Appear><Heading size={4}>2 Summarise, 2 Categorise</Heading></Appear>          
+
+          <Notes>
+            <ul>
+              <li>Anyway, you'll have your own fave apps, tools, whatever. But regardless of technology, there are a few techniques...</li>
+            </ul>
+          </Notes>
         </Slide>
 
         <Slide>
           <Heading>TIL</Heading>
+
+          <Notes>
+            <ul>
+              <li>You'll benefit from it either way, but I want to encourage you...</li>
+              <li>Does that sound scary?</li>
+            </ul>
+          </Notes>
         </Slide>
 
         <Slide>
@@ -295,14 +455,35 @@ export default class Presentation extends React.Component {
           <Appear><Heading size={4}>What If I'm Wrong?</Heading></Appear>
           <Appear><Heading size={4}>What If I'm Right?</Heading></Appear>
           <Appear><Heading size={4}>What If Nobody Cares?</Heading></Appear>
+
+          <Notes>
+            <ul>
+              <li>Right: here's the catch22...</li>
+            </ul>
+          </Notes>
+
         </Slide>
 
         <Slide bgImage={images.knowledge} />
 
-        <Slide></Slide>
+        <Slide>
+          <Notes>
+            <ul>
+              <li>I mentioned before about Memex trails, and the idea of being able to follow in someone else's footsteps/. I find it interesting that many of our formal collaborative...</li>
+            </ul>
+          </Notes>
+        </Slide>
 
         <Slide>
           <Heading>WIP</Heading>
+          
+          <Notes>
+            <ul>
+              <li>If you say "I tried these 3 different approaches..."</li>
+              <li>Why don't we do this more? Again, similar reasons as before...</li>
+            </ul>
+          </Notes>
+
         </Slide>
 
         <Slide>
@@ -311,14 +492,35 @@ export default class Presentation extends React.Component {
           <Appear><Heading size={4}>What If I'm Wrong?</Heading></Appear>
           <Appear><Heading size={4}>What If I'm Right?</Heading></Appear>
           <Appear><Heading size={4}>What If Nobody Cares?</Heading></Appear>
+
+          <Notes>
+            <ul>
+              <li>Be humble, and thank the person.... at very least, you've shown your team...</li>
+              <li>Don't worry too much when you hear this person. They are small-minded, short-sighted...</li>
+              <li>Now this one actually is a valid concern... Here's how we do it at X-Team</li>
+            </ul>
+          </Notes>
+
         </Slide>
 
         <Slide>
           <Heading>Tip: journal channels</Heading>
+
+          <Notes>
+            <ul>
+              <li>Think about how you can leave a trail for your team mates to follow. Because remember:</li>
+            </ul>
+          </Notes>
         </Slide>
 
         <Slide>
           <Heading><em>&ldquo;The team that WIPs together ships together&rdquo;</em></Heading>
+
+          <Notes>
+            <ul>
+              <li>There are many other techniques like this, I won't go into them all here. But I'll be happy to talk more later...</li>
+            </ul>
+          </Notes>
         </Slide>
 
         <Slide>
@@ -339,6 +541,15 @@ export default class Presentation extends React.Component {
               </div>
             </Appear>
           </div>
+
+          <Notes>
+            <ul>
+              <li>Programming is a creative form that overlaps many principles...</li>
+              <li>So I encourage you to look for ways to write creatively...</li>
+              <li>Most of all, just start writing...</li>
+              <li>All it takes is one person to start...</li>
+            </ul>
+          </Notes>
         </Slide>
 
         <Slide>
